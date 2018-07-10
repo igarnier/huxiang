bc5c0b03e8b069a4f5ce56c8aef9a435a53c63a5
Ilias <ilias.gar@gmail.com>
2018-07-10 13:18:32 +0200

    cleanup

---
+++ examples/ping_pong/pong.ml
@@ -0,0 +1,32 @@
+open Batteries
+open Huxiang.Types
+
+module PongProcess : Process =
+struct
+
+  module I = Messages.PingMsg
+  module O = Messages.PongMsg
+               
+  type state = unit
+
+  let initial_state   = ()
+  let initial_message = None
+
+  let transition state (I.Ping i) =
+    Lwt.return (state, Some (O.Pong i))
+      
+end
+
+module PongNode = Huxiang.Node.Make(PongProcess)
+
+let _ =
+  let () = Lwt_log.add_rule "*" Lwt_log.Debug in
+  Lwt_log.default := (Lwt_log.channel
+                        ~template:"$(date).$(milliseconds) [$(level)] $(message)"
+                        ~channel:Lwt_io.stderr
+                        ~close_mode:`Keep
+                        ());  
+  PongNode.start
+    ~ingoing:["tcp://127.0.0.1:5557"]
+    ~outgoing:["tcp://127.0.0.1:5556"]
+
--- examples/ping_pong/server.ml
+++ examples/ping_pong/server.ml
@@ -12,7 +12,7 @@ struct
     tstamp : float
   }
   
-  type state = (I.client_id * (float * int)) list
+  type state = (I.msg * (float * int)) list
 
   let initial_state = []
                       
@@ -20,31 +20,10 @@ struct
 
   let transition state in_msg =
     match in_msg with
-    | I.Hello { client_id; nonce } ->
-      Lwt_io.eprintf "-> hello %s %d\n" client_id nonce;%lwt
-      (match List.assoc_opt client_id state with
-       | None ->
-         let t = Unix.gettimeofday () in
-         let state = (client_id, (t, nonce)) :: state in
-         Lwt_io.eprintf "<- ack(hello %s %d)\n" client_id nonce;%lwt
-         Lwt.return (state, Some (O.Ack in_msg))
-       | Some _ ->
-         Lwt.fail_with "transition: client already Hello'd")
-    | I.Alive { client_id; nonce } ->
-      Lwt_io.eprintf "-> alive %s %d\n" client_id nonce;%lwt
-      (match List.assoc_opt client_id state with
-       | None ->
-         Lwt.fail_with "transition: client didn't Hello"
-       | Some (t', nonce') ->
-         let t = Unix.gettimeofday () in
-         if nonce' >= nonce || (t -. t' >= 30.0) then
-           (Lwt_io.print "transition: server died\n";%lwt
-            Lwt.return (state, (Some O.Die)))
-         else
-           (Lwt_io.eprintf "<- ack %s %d\n" client_id nonce;%lwt
-            Lwt.return (state, Some (O.Ack in_msg)))
-      )
-
+    | I.Ping msg ->
+      failwith ""
+        
+    
 end
 
 module Server = Huxiang.Node_lwt.Make(Protocol)
