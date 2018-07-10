0d3ec7ab4ff39814e3ecba7d7e09a70e21a65507
Ilias <ilias.gar@gmail.com>
2018-07-10 13:16:35 +0200

    modified example

---
+++ examples/ping_pong/ping.ml
@@ -0,0 +1,41 @@
+open Batteries
+open Huxiang.Types
+
+module PingProcess =
+struct
+
+  module I = Messages.PongMsg
+  module O = Messages.PingMsg
+               
+  type state =
+    | Alive of { counter : int }
+    | Dead
+
+  let initial_state   = Alive { counter = 0 }
+  let initial_message = Some (O.Ping 0)
+
+  let transition state (I.Pong i) =
+    match state with
+    | Dead              -> Lwt.return (Dead, None)
+    | Alive { counter } ->
+      if i = counter then
+        let state = Alive { counter = counter + 1 } in
+        Lwt.return (state, Some (O.Ping (counter + 1)))
+      else
+        Lwt.return (Dead, None)
+      
+end
+
+module PingNode = Huxiang.Node.Make(PingProcess)
+
+let _ =
+  let () = Lwt_log.add_rule "*" Lwt_log.Debug in
+  Lwt_log.default := (Lwt_log.channel
+                        ~template:"$(date).$(milliseconds) [$(level)] $(message)"
+                        ~channel:Lwt_io.stderr
+                        ~close_mode:`Keep
+                        ());  
+  PingNode.start
+    ~ingoing:["tcp://127.0.0.1:5556"]
+    ~outgoing:["tcp://127.0.0.1:5557"]
+
