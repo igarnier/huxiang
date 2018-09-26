810ab134eb35ed72a6ba4c9e05829ea45a479cdc
Ilias <ilias.gar@gmail.com>
2018-09-26 17:30:13 +0200

    Added conversion from/to byte/bigarray

--- lib/utils.ml
+++ lib/utils.ml
@@ -12,3 +12,15 @@ let map_reader (type t u) (r : t Type_class.reader) (f : t -> u) : u Type_class.
     Type_class.read = (fun buf ~pos_ref -> f (r.Type_class.read buf ~pos_ref));
     vtag_read = (fun buf ~pos_ref tag -> f (r.vtag_read buf ~pos_ref tag))
   }
+
+let bytes_to_buffer bytes =
+  let len = Bytes.length bytes in
+  let b = Common.create_buf len in
+  Common.blit_bytes_buf bytes b ~len;
+  b
+  
+let buffer_to_bytes buf =
+  let len   = Common.buf_len buf in
+  let bytes = Bytes.create len in
+  Common.blit_buf_bytes buf bytes ~len;
+  bytes
--- lib/utils.mli
+++ lib/utils.mli
@@ -1,2 +1,5 @@
 val map_writer : 'a Bin_prot.Type_class.writer -> ('b -> 'a) -> 'b Bin_prot.Type_class.writer
 val map_reader : 'a Bin_prot.Type_class.reader -> ('a -> 'b) -> 'b Bin_prot.Type_class.reader
+
+val bytes_to_buffer : Bytes.t -> Bin_prot.Common.buf
+val buffer_to_bytes : Bin_prot.Common.buf -> Bytes.t
