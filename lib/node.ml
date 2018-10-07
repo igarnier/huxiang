2399878f0c635018cf27e9720e9bd077d411f979
Ilias <ilias.gar@gmail.com>
2018-10-07 21:58:20 +0200

    Network code cleanup

--- lib/node.ml
+++ lib/node.ml
@@ -1,9 +1,44 @@
 open Batteries
 open Zmq
 
+(* Aux functions *)
+let lwt_debug fname msg =
+  Lwt_log.debug_f "%s: %s" fname msg
+
+let lwt_info fname msg =
+  Lwt_log.info_f "%s: %s" fname msg
+
+let lwt_fail fname msg =
+  Lwt.fail_with @@ fname^": "^msg
+
+let lwt_fail_exn fname msg exn =
+  let s   = Printexc.to_string exn in
+  let err =
+    Printf.sprintf "%s: exception %s raised (%s)" fname s msg
+  in
+  Lwt.fail_with err
+
+let fail_exn fname msg exn =
+  let s   = Printexc.to_string exn in
+  let err =
+    Printf.sprintf "%s: exception %s raised (%s)" fname s msg
+  in
+  failwith err
+
+
 module LwtSocket = Zmq_lwt.Socket
 
-type network_map = Address.t -> string
+type address = string
+
+type input_socket =
+  | Subscribe  of address
+  | ReliableIn of address
+
+type output_socket =
+  | Publish     of address
+  | ReliableOut of address
+
+type network_map = Address.t -> address
 
 open Bin_prot.Std
 
@@ -12,12 +47,14 @@ type message =
       msg : NetProcess.Input.t;
       uid : int64
     }
-[@@deriving bin_io]
+[@@deriving bin_io, show]
 
+(* serialise message *)
 let message_to_bytes msg =
-  let buf   = Bin_prot.Utils.bin_dump bin_writer_message msg in
+  let buf = Bin_prot.Utils.bin_dump bin_writer_message msg in
   Utils.buffer_to_bytes buf
 
+(* deserialise message *)
 let message_from_bytes bytes =
   let buffer = Utils.bytes_to_buffer bytes in
   bin_reader_message.read buffer ~pos_ref:(ref 0)
@@ -25,145 +62,170 @@ let message_from_bytes bytes =
 type routing =
   | Dynamic of (Address.t -> [`Dealer] LwtSocket.t)
 
+type node_state =
+  {
+    ingoing  : [`Dealer | `Sub] LwtSocket.t list;
+    routing  : routing;
+    iqueue   : NetProcess.input list Lwt_mvar.t;
+    oqueue   : NetProcess.output Lwt_mvar.t;
+    skey     : Crypto.Secret.t;
+    pkey     : Crypto.Public.t
+  }
+
+(* Reader thread *)
+
+let message_from_string (str : string) =
+  let bytes = Bytes.of_string str in
+  message_from_bytes bytes
+
+let read_sockets sockets =
+  let     reads  = List.map LwtSocket.recv sockets in  
+  let%lwt result = Lwt.npick reads in
+  let     result = 
+    List.map (message_from_string %> (fun (Msg { msg; _}) -> msg)) result
+  in
+  Lwt.return result
+
+let reader_thread { ingoing; iqueue; _ } =
+  let fname = "huxiang/node/reader_thread" in
+  let rec loop () =
+    let%lwt messages = read_sockets ingoing in
+    match messages with
+    | [] ->
+      lwt_debug fname "read no message";%lwt
+      loop ()
+    | _ ->
+      lwt_debug fname "read some messages";%lwt
+      let%lwt previous_msgs = Lwt_mvar.take iqueue in
+      lwt_debug fname "put them in the iqueue";%lwt
+      Lwt_mvar.put iqueue (previous_msgs @ messages);%lwt
+      loop ()
+  in
+  loop ()
+
+(* Writer thread *)
+
+let message_to_string msg =
+  let bytes = message_to_bytes msg in
+  Bytes.to_string bytes
+
+let write_socket msg (consumer : 'a LwtSocket.t) =
+  let fname = "huxiang/node/write_socket" in
+  let serialized = message_to_string msg in
+  LwtSocket.send consumer serialized;%lwt
+  lwt_debug fname ("message sent: "^(show_message msg))
+
+let write_to_outgoing { Address.msg; dests } uid (Dynamic table) skey pkey =
+  (* let fname = "huxiang/node/write_to_outgoing" in *)
+  Lwt_list.iter_p (fun address ->
+      let socket = table address in
+      let msg  = 
+        let module Cred : Crypto.Credentials = 
+        struct 
+          let public_key = pkey
+          let secret_key = skey
+        end
+        in
+        let open NetProcess in
+        let data = Crypto.Signed.pack msg (module Types.HuxiangBytes) (module Cred) in
+        Input.Signed { data }
+      in
+      write_socket (Msg { msg; uid }) socket
+    ) dests
+
+let writer_thread { routing; oqueue; skey; pkey; _ } =
+  let rec loop uid =
+    let%lwt msg = Lwt_mvar.take oqueue in
+    write_to_outgoing msg uid routing skey pkey;%lwt
+    loop Int64.(uid + one)
+  in
+  loop Int64.zero
+ 
+
 module Make(P : NetProcess.S) =
 struct
-      
+
   type t =
     {
-      ingoing  : [`Dealer] LwtSocket.t; (* recv msg; send ack *)
-      routing  : routing;
-      mqueue   : NetProcess.output Lwt_mvar.t;
-      process  : P.state NetProcess.t;
-      skey     : Crypto.Secret.t;
-      pkey     : Crypto.Public.t
+      node_state : node_state;
+      process    : P.state NetProcess.t
     }
-    
-  let print_msg msg =
-    match msg with
-    | Msg { uid; _ } ->
-      Printf.sprintf "msg(%Ld)" uid
-  
-  let lwt_fail fname msg =
-    Lwt.fail_with @@ fname^": "^msg
+      
+  let is_input_available { node_state = { iqueue; _ }; _ } =
+    let%lwt inputs = Lwt_mvar.take iqueue in
+    let result = not (List.is_empty inputs) in
+    Lwt_mvar.put iqueue inputs;%lwt
+    Lwt.return result
 
-  let lwt_debug fname msg =
-    Lwt_log.debug_f "%s: %s" fname msg
+  let take_one_input { node_state = { iqueue; _ }; _ } =
+    let fname = "huxiang/node/take_one_input" in
+    let%lwt inputs = Lwt_mvar.take iqueue in
+    match inputs with
+    | [] ->
+      Lwt_mvar.put iqueue [];%lwt
+      lwt_fail fname "queue should not be empty, bug found"
+    | input :: tl ->
+      Lwt_mvar.put iqueue tl;%lwt
+      Lwt.return input
 
-  let input str =
-    let bytes = Bytes.of_string str in
-    Lwt.return (message_from_bytes bytes)
-          
-  let output msg =
-    let bytes = message_to_bytes msg in
-    Bytes.to_string bytes
-
-  let read_msg provider =
-    let fname = "huxiang/node/read_and_ack" in
-    let%lwt str = LwtSocket.recv provider in
-    let%lwt msg = input str in
-    lwt_debug fname ("read message "^(print_msg msg));%lwt
-    match msg with
-    | Msg { msg; _ } -> Lwt.return msg
-
-  let write_msg(*and_get_acked*) out_msg (consumer : [`Dealer] LwtSocket.t) =
-    let fname = "huxiang/node/write_and_get_acked" in
-    let%lwt serialized =
-      try%lwt Lwt.return (output out_msg)
-      with exn ->
-        let message = 
-          "error caught during serialization: "^(Printexc.to_string exn) 
-        in
-        lwt_fail fname message
-    in
-    LwtSocket.send consumer serialized;%lwt
-    lwt_debug fname ("message sent: "^(print_msg out_msg))
-
-  let write_to_outgoing { Address.msg; dests } uid (Dynamic table) skey pkey =
-    let fname = "huxiang/node/write_to_outgoing" in
-    Lwt_list.iter_p (fun address ->
-        let%lwt socket = 
-          try%lwt Lwt.return (table address)
-          with
-          | exn ->
-            let message = 
-              "error in network table computation: "^(Printexc.to_string exn)
-            in
-            lwt_debug fname message;%lwt
-            Lwt.fail exn
-        in
-        let msg  = 
-          let module Cred : Crypto.Credentials = 
-          struct 
-            let public_key = pkey
-            let secret_key = skey
-          end
-          in
-          let open NetProcess in
-          let data = Crypto.Signed.pack msg (module Types.HuxiangBytes) (module Cred) in
-          Input.Signed { data }
-        in
-        write_msg (Msg { uid; msg }) socket
-      ) dests
-        
-  let reader_thread { ingoing; mqueue; process; _ } =
-    let fname = "huxiang/node/reader_thread" in
+  let process_thread state =
+    let fname = "huxiang/node/process_thread" in
     let rec loop process =
-      lwt_debug fname "loop entered";%lwt
-      let continue out next = (* factorised continuation *)
-        (match out with
-         | None -> Lwt.return ()
-         | Some out ->
-           Lwt_mvar.put mqueue out;%lwt
-           lwt_debug fname "put"
-        );%lwt
-        loop next
-      in
-      (* TODO: Here we need to parameterize the node by a scheduler /!!!!\*)
-      match Process.evolve process with
-      | (Input transition) :: _ ->
-        lwt_debug fname "Input transition";%lwt
-        (let%lwt msg = read_msg ingoing in
-         lwt_debug fname "read";%lwt
-         let%lwt { Process.output; next } =
-           try%lwt transition msg with
-           | exn ->
-             let message = 
-               "error caught in Input transition: "^(Printexc.to_string exn) 
-             in
-             lwt_debug fname message;%lwt
-             Lwt.fail exn
-         in
-         continue output next
-        )
-      | (NoInput transition) :: _ ->
-        lwt_debug fname "NoInput transition";%lwt
-        let%lwt { Process.output; next } =
-          try%lwt transition with
-          | exn ->
-             let message = 
-               "error caught in NoInput transition: "^(Printexc.to_string exn)
-             in
-             lwt_debug fname message;%lwt
-             Lwt.fail exn
-        in
-        continue output next
+      let transitions = Process.evolve process in
+      match transitions with
       | [] ->
-        Lwt_log.log ~level:Info "reader: Stop state reached";%lwt
+        lwt_info fname "final state reached";%lwt
         Lwt.return ()
+      | _ ->
+        begin
+          let%lwt input_present = is_input_available state in
+          let transitions =
+            (* filter out inputful transitions when there are no
+               inputs available. *)
+            List.filter (function
+                | Process.Input _ -> input_present
+                | _               -> true
+              ) (Process.evolve process)
+          in
+          match transitions with
+          | [] ->
+            (* No transitions can be played now. We should wake up
+               this thread whenever some input is received. *)
+            lwt_debug fname "no playable transition";%lwt
+            Lwt.pause ();%lwt
+            loop process
+          | _ -> 
+            begin
+              (* pick a transition at random, play it *)
+              let tr = Random.choice (List.enum transitions) in
+              match tr with
+              | Input transition ->
+                lwt_debug fname "playing Input transition";%lwt
+                let%lwt msg = take_one_input state in
+                let%lwt { Process.output; next } =
+                  try%lwt transition msg with
+                  | exn ->
+                    lwt_fail_exn fname "error caught in Input transition" exn
+                in
+                continue output next
+              | NoInput transition ->
+                lwt_debug fname "playing NoInput transition";%lwt
+                let%lwt { Process.output; next } =
+                  try%lwt transition with
+                  | exn ->
+                    lwt_fail_exn fname "error caught in NoInput transition" exn
+                in
+                continue output next
+            end
+        end
+    and continue out next = (* factorised continuation *)
+      (match out with
+       | None     -> Lwt.return ()
+       | Some out -> Lwt_mvar.put state.node_state.oqueue out
+      );%lwt
+      loop next
     in
-    loop process
-
-  let writer_thread { routing; mqueue; skey; pkey; _ } =
-    let fname = "huxiang/node/writer_thread" in
-    let rec loop uid =
-      lwt_debug fname "loop entered";%lwt
-      let%lwt msg = Lwt_mvar.take mqueue in
-      lwt_debug fname "taken";%lwt
-      write_to_outgoing msg uid routing skey pkey;%lwt
-      lwt_debug fname "written";%lwt
-      loop Int64.(uid + one)
-    in
-    loop 0L
+    loop state.process
 
   let with_context f =
     let c = Context.create () in
@@ -172,58 +234,65 @@ struct
     r
 
   let close ingoing outgoing =
-    Socket.close (LwtSocket.to_socket ingoing);
+    List.iter (fun x -> Socket.close (LwtSocket.to_socket x)) ingoing;
     List.iter (fun s ->
         let s = LwtSocket.to_socket s in
         Socket.close s
       ) outgoing
       
   let get_socket ctx table address =
+    let fname = "huxiang/node/get_socket" in
     match Hashtbl.find_option table address with
     | None ->
-      let sck = Socket.(create ctx dealer) in
-      (try Socket.connect sck address with
-       | exn ->
-         let s = Printexc.to_string exn in
-         failwith @@
-         "huxiang/node/get_socket: exception "^s^
-         " raised while binding addr "^
-         address);
+      let sck = 
+        try Socket.create ctx Socket.dealer 
+        with exn -> fail_exn fname "Socket.create" exn
+      in
+      let () =
+        try Socket.connect sck address 
+        with exn -> fail_exn fname ("Socket.connect "^address) exn
+      in
       let sck = LwtSocket.of_socket sck in
       Hashtbl.add table address sck;
       sck
     | Some sck ->
       sck
 
-  let start ~listening ~network_map ~skey ~pkey =
+  let create_in_socket ctx input_socket =
+    match input_socket with 
+    | Subscribe addr ->
+      let open Socket in
+      let sck = create ctx sub in
+      Socket.bind sck addr;
+      LwtSocket.of_socket sck
+    | ReliableIn addr ->
+      let open Socket in
+      let sck = create ctx dealer in
+      Socket.bind sck addr;
+      LwtSocket.of_socket sck
+          
+  let start ~(listening : input_socket list) ~network_map ~skey ~pkey =
+    (* let fname = "huxiang/node/start" in *)
     with_context (fun ctx ->
-        let ingoing =
-          let sck = Socket.(create ctx dealer) in
-          (try Socket.bind sck listening with
-           | exn ->
-             let s = Printexc.to_string exn in
-             failwith @@
-             "Could not connect to address "^listening^": "^
-             "error "^s^" caught");
-          LwtSocket.of_socket sck
-        in
+        let ingoing = List.map (create_in_socket ctx) listening in
         let table = Hashtbl.create 30 in
-        let record =
-          {
-            ingoing;
-            routing = Dynamic (fun x -> get_socket ctx table (network_map x));
-            mqueue  = Lwt_mvar.create_empty ();
-            process = P.thread;
-            skey;
-            pkey
-          }
-        in
+        let node_state = {
+          ingoing;
+          routing = Dynamic (fun x -> get_socket ctx table (network_map x));
+          iqueue  = Lwt_mvar.create [];
+          oqueue  = Lwt_mvar.create_empty ();
+          skey;
+          pkey
+        } in
+        let whole_state = { node_state; process = P.thread } in
         let program =
           Lwt.finalize
             (fun () ->
                Lwt_log.log ~level:Info "Starting node!";%lwt
-               Lwt.join [ reader_thread record;
-                          writer_thread record ])
+               Lwt.join [ reader_thread node_state;
+                          writer_thread node_state;
+                          process_thread whole_state
+                        ])
             (fun () ->
                let outgoing = Hashtbl.values table in
                let outgoing = List.of_enum outgoing in
--- lib/node.mli
+++ lib/node.mli
@@ -1,10 +1,37 @@
-type network_map = Address.t -> string
+(** A network node is a small server executing a communicating state machine.
+    It can have several sockets on input and output. Sockets can be of two
+    types: subscribe/publish (sub/pub) or reliable. In sub/pub mode, nodes
+    on the receiving end subscribe to a flux of messages emitted by another
+    node. A "Publish" socket can only be connected to (possibly several)
+    "Subscribe" ones. Note that is there are no subscribers, messages emitted
+    will be lost.
+    
+    Reliable sockets on the other hand never lose messages. Messages emitted
+    on a "ReliableOut" socket will be queued until a peer connects (with
+    a "ReliableIn" socket). Writing to a "ReliableOut" socket can block if
+    the queue is full.
+
+    It is up to each node to correctly setup socket types matching that of
+    network peers.
+*)
+
+type address = string
+
+type input_socket =
+  | Subscribe  of address
+  | ReliableIn of address
+
+type output_socket =
+  | Publish     of address
+  | ReliableOut of address
+
+type network_map = Address.t -> address
 
 module Make(P : NetProcess.S) :
 sig
 
   val start : 
-    listening:string -> 
+    listening:input_socket list -> 
     network_map:network_map ->
     skey:Crypto.Secret.t ->
     pkey:Crypto.Public.t ->
